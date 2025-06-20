bf72737cc0988569d7a9c54ef479be5f6189439f
RickyRTM <rickyrtm@gmail.com>
2025-06-19 23:51:19 -0600

    A ver el service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,40 +1,22 @@
-const odsquiz = 'odsquiz-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/js/app.js',
-  '/manifest.json',
-  'images/ChatGPT Image 19 jun 2025, 08_57_38 p.m..png',
-];
+const GBAR = ""
+const appShell = [
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(odsquiz).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+]
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== odsquiz) {
-            return caches.delete(key);
-          }
+
+
+self.addEventListener("install", installEvent =>{
+    installEvent.waitUntil(
+        caches.open(appCacheKey).then(cache =>{
+            cache.addAll(appShell);
         })
-      );
-    })
-  );
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    }).catch(() => {
-    })
-  );
+self.addEventListene("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
 });
