1e2c9b59320ac25dd9408a2da461fc8507e0ad07
RickyRTM <rickyrtm@gmail.com>
2025-06-19 23:26:54 -0600

    Desde la casa de Ricky

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,26 @@
-const staticDevCoffe = "dev-coffee-site-v1"
+const CacheKeyHuerto = "GBAR";
 
-const appShell = 
+const appShell =[
+    "/",
+    "/js/app.js",
+    "/images/ChatGPT Image 19 jun 2025, 08_57_38 p.m..png",
+    "index.js",
+    "styles.css",
+    "index.html"
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(CacheKeyHuerto).then(cache => {
+            cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
+});
